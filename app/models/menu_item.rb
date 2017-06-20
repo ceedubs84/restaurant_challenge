9d61d1eb5eb76f547198dc9b788c7275f24d0357
Chris Woodside <christopherwoodside@gmail.com>
2017-06-19 20:39:39 -0500

    Added association, validation, and methods to MenuItem model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,16 @@
 class MenuItem < ApplicationRecord
-  belongs_to :menu_item
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, presence: true
+  validates :description, uniqueness: true
+
+  validates :price, numericality: { greater_than: 0 }
+
+  def tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
 end
