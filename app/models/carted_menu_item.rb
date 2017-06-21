260db6550720dcf584d842da28e6fee843b9a0d9
Chris Woodside <christopherwoodside@gmail.com>
2017-06-21 14:24:54 -0500

    Add validations to CMI and add CMI resources to routes

--- app/models/carted_menu_item.rb
+++ app/models/carted_menu_item.rb
@@ -1,2 +1,11 @@
 class CartedMenuItem < ApplicationRecord
+  belongs_to :user
+  belongs_to :menu_item
+
+  validates :quantity, presence: true
+  validates :quantity, numericality: true
+
+  def subtotal
+    quantity * product.price
+  end
 end
