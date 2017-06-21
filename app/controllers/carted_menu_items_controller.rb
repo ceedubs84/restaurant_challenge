0bbe41f1870022954b9f3f5812ba0dac9a518795
Chris Woodside <christopherwoodside@gmail.com>
2017-06-21 18:28:05 -0500

    Add corrected before_action to controllers

--- app/controllers/carted_menu_items_controller.rb
+++ app/controllers/carted_menu_items_controller.rb
@@ -1,6 +1,6 @@
 class CartedMenuItemsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-  before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!, only: [:create]
 
   def index
     @carted_menu_items = current_user.carted_menu_items.where(status: 'carted')
--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,5 @@
 class MenuItemsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   before_action :authenticate_admin!, only: [:new, :create, :update]
 
   def index
