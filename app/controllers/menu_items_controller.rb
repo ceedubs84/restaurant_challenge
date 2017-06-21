05e5e51f9613901fb4e2c7a11f10d9889ff2eb6d
Chris Woodside <christopherwoodside@gmail.com>
2017-06-21 14:27:32 -0500

    Add skip_before_filter to MI controller, corrected form configuration for MI Index, and migration for CMI

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,4 +1,5 @@
 class MenuItemsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_action :authenticate_admin!, only: [:new, :create, :update]
 
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620003225) do
+ActiveRecord::Schema.define(version: 20170621174241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carted_menu_items", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "menu_item_id"
+    t.integer  "quantity"
+    t.string   "status"
+    t.integer  "order_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "menu_items", force: :cascade do |t|
     t.string   "name"
