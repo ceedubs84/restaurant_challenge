e1ae11c59cabeffc3f5836d6ca4b8737dbe43d90
Chris Woodside <christopherwoodside@gmail.com>
2017-06-23 13:04:20 -0500

    Added Optional:true to Order association in CMI model

--- app/models/carted_menu_item.rb
+++ app/models/carted_menu_item.rb
@@ -1,10 +1,11 @@
 class CartedMenuItem < ApplicationRecord
   belongs_to :user
   belongs_to :menu_item
-  belongs_to :order
+  belongs_to :order, optional: true
 
   validates :quantity, presence: true
-  validates :quantity, numericality: true
+  validates :quantity, numericality: { greater_than: 0, only_integer: true }
+  validates :order, presence: true, allow_nil: true
 
   def subtotal
     quantity * menu_item.price
