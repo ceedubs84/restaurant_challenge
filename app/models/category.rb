2d9047ac1488ff7d1439e0a1b0840f773d5d7bf0
Chris Woodside <christopherwoodside@gmail.com>
2017-06-24 13:22:28 -0500

    Add associations for Category and MenuItemCategory models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :menu_item_categories
+  has_many :menu_items, through: :menu_item_categories
 end
--- app/models/menu_item_category.rb
+++ app/models/menu_item_category.rb
@@ -1,2 +1,4 @@
 class MenuItemCategory < ApplicationRecord
+  belongs_to :category
+  belongs_to :menu_item
 end
