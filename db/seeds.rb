80d0132051111bd4922e556874bb17947feb6012
Chris Woodside <christopherwoodside@gmail.com>
2017-06-27 20:49:58 -0500

    Create seed file with MenItems and Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  MenuItem.create(
+    name: Faker::Food.ingredient,
+    description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price,
+  )
+end
+menu_item_categories = ['Breakfast', 'Lunch', 'Dinner', 'Dessert', 'Appetizer', 'Drink', 'Side']
+
+menu_item_categories.each do |category|
+  Category.create(name: category)
+end
