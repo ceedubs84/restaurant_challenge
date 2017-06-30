3a490ea9ce005ed74dd780aab64c6e7f60655e86
Chris Woodside <christopherwoodside@gmail.com>
2017-06-30 10:18:27 -0500

    Add password_confirmation field to User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name 'Joe Test'
     sequence(:email) { |n| "testuser#{n}@test.com" }
     password 'password'
+    password_confirmation 'password'
     admin 'false'
 
     factory :admin do
