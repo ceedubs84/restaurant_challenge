ee8de4e22a39a09d315e904a41234e1edd6c0705
Chris Woodside <christopherwoodside@gmail.com>
2017-06-29 19:40:09 -0500

    Add MenuItem factory and model spec

--- spec/factories/menu_items.rb
+++ spec/factories/menu_items.rb
@@ -1,5 +1,20 @@
 FactoryGirl.define do
+  factory :category do
+    name Faker::Commerce.department(1, true)
+  end
+
   factory :menu_item do
-    
+    name Faker::Food.ingredient
+    description Faker::Lorem.sentence
+    price Faker::Commerce.price
+
+    factory :menu_item_with_categories do
+      transient do
+        categories_count 2
+      end
+      after(:create) do |menu_item, evaluator|
+        create_list(:category, evaluator.categories_count, menu_items: [menu_item])
+      end
+    end
   end
 end
--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe MenuItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid MenuItem factory' do
+    expect(FactoryGirl.build(:menu_item_with_categories)).to be_valid
+  end
+
+  describe '#tax' do
+    it 'returns the tax of the menu item' do
+      menu_item = build(:menu_item_with_categories, price: 10.0)
+      expect(menu_item.tax.to_f).to eq(0.9)
+    end
+  end
+
+  describe '#total' do
+    it 'returns the total of the price of the menu item plus the tax' do
+      menu_item = build(:menu_item_with_categories, price: 10.0)
+      expect(menu_item.total).to eq(10.9)
+    end
+  end
 end
