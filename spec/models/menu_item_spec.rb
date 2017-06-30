30a8133e9870ef378d08218b2cfd822333a10856
Chris Woodside <christopherwoodside@gmail.com>
2017-06-29 22:00:38 -0500

    Add MenuItem spec negative test

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe MenuItem, type: :model do
-  it 'has a valid MenuItem factory' do
-    expect(FactoryGirl.build(:menu_item_with_categories)).to be_valid
+  describe 'validations' do
+    it 'has a valid MenuItem factory' do
+      expect(FactoryGirl.build(:menu_item_with_categories)).to be_valid
+    end
+
+    it 'is invalid if menu item already exists with the same name' do
+      mi1 = create(:menu_item, name: 'Candy Bar', price: 3.0)
+      mi2 = build(:menu_item, name: 'Candy Bar', price: 2.0)
+      expect(mi2).to_not be_valid
+    end
   end
 
   describe '#tax' do
