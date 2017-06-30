e0c97e365d446fcea9c8be5aa35fd267ca0256dc
Chris Woodside <christopherwoodside@gmail.com>
2017-06-29 22:37:13 -0500

    Update User validations and add invalid path to spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :carted_menu_items
   has_many :orders
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'has a valid User factory' do
-    expect(FactoryGirl.build(:user)).to be_valid
-  end
+  describe '#validations' do
+    it 'has a valid User factory' do
+      expect(FactoryGirl.build(:user)).to be_valid
+    end
 
-  it 'has a valid Admin factory' do
-    expect(FactoryGirl.build(:admin)).to be_valid
+    it 'has a valid Admin factory' do
+      expect(FactoryGirl.build(:admin)).to be_valid
+    end
+
+    it 'is invalid if menu item already exists with the same name' do
+      user1 = create(:user, email: 'joe_user@test.com')
+      user2 = build(:user, email: 'joe_user@test.com')
+      expect(user2).to_not be_valid
+    end
   end
 end
