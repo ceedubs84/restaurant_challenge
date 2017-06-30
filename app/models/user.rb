41f616759b6496adb543424a0a7b3cd915a465b4
Chris Woodside <christopherwoodside@gmail.com>
2017-06-30 10:15:14 -0500

    Add name and password validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :carted_menu_items
   has_many :orders
 
+  validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :password, presence: true
 end
