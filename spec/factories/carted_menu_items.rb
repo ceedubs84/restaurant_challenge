7314a0b6b32d1e4200e37292eedd69b65ef05251
Chris Woodside <christopherwoodside@gmail.com>
2017-06-29 19:41:21 -0500

    Add CartedMenuItem factory and model spec

--- spec/factories/carted_menu_items.rb
+++ spec/factories/carted_menu_items.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :carted_menu_item do
-    
+    user
+    menu_item
+    quantity 5
+    status 'carted'
   end
 end
--- spec/models/carted_menu_item_spec.rb
+++ spec/models/carted_menu_item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe CartedMenuItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid CartedMenuItem factory' do
+    expect(FactoryGirl.build(:carted_menu_item)).to be_valid
+  end
+
+  describe '#subtotal' do
+    it 'returns the subtotal of the price mulitiplied by the quatity' do
+      carted_menu_item = build(:carted_menu_item, quantity: 10)
+      price = carted_menu_item.menu_item.price
+      expect(carted_menu_item.subtotal).to eq(price * 10)
+    end
+  end
 end
